947eb0108e45d792bd059aa04a3b446215896772
halli21 <haraldurf21@ru.is>
2024-03-27 17:52:19 +0000

    code

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -9,20 +9,11 @@ export default function Navbar() {
                 <label htmlFor="my-drawer" className="btn btn-primary drawer-button">Open drawer</label>
             </div> 
 
-            
             <div className="drawer-side">
                 <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
                 <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-
-                    <li>
-                        <Link href="/">Home</Link>
-                    </li>
-      
-                    <li>
-                        <Link href="/devices">Devices</Link>
-                    </li>
-                    
-                
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/devices">Devices</Link></li>
                 </ul>
             </div>
         </div>

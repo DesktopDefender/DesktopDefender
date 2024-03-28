78bb241dbae920e2f84a8068b80dac5f3868c4ad
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-03-28 14:26:16 +0000

    lint + format

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,22 +1,32 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Navbar() {
-    return (
-      <nav>
-        <div className="drawer">
-            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content">
-                <label htmlFor="my-drawer" className="btn btn-primary drawer-button">Open drawer</label>
-            </div> 
-
-            <div className="drawer-side">
-                <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-                    <li><Link href="/">Home</Link></li>
-                    <li><Link href="/devices">Devices</Link></li>
-                </ul>
-            </div>
+  return (
+    <nav>
+      <div className="drawer">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          <label htmlFor="my-drawer" className="btn btn-primary drawer-button">
+            Open drawer
+          </label>
         </div>
-      </nav>
-    );
+
+        <div className="drawer-side">
+          <label
+            htmlFor="my-drawer"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          />
+          <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/devices">Devices</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 

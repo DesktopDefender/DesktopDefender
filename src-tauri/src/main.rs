33617a92f615dce9f07dba16ccdbb437b8d615fe
Lsig <logis21@ru.is>
2024-03-28 13:49:38 +0000

    feat(monitor): Configure tauri

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+extern crate pcap;
+extern crate pnet;
+
+mod network_monitor;
+use crate::network_monitor::monitor::listen_to_traffic;
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![listen_to_traffic])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }

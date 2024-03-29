dc2e78ea1942d00628d2cf8747f10b1c0acb9f25
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-03-29 11:22:14 +0000

    components dir moved to src/ + biome organized imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
+import Navbar from "@/components/Navbar";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
---
+++ src/app/router/page.tsx
@@ -0,0 +1,7 @@
+export default function Router() {
+  return (
+    <div className="min-h-screen">
+      <p>hello, router</p>
+    </div>
+  );
+}

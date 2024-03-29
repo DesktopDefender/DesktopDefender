ba3c3a7090147108e902709eb3df432c7cd2e7d2
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-03-29 12:56:19 +0000

    router page invokes rust greet function

--- src/app/router/page.tsx
+++ src/app/router/page.tsx
@@ -1,7 +1,43 @@
+"use client";
+
+import { invoke } from "@tauri-apps/api/tauri";
+import { useEffect, useState } from "react";
+
 export default function Router() {
+  const [greet, setGreet] = useState("");
+
+  const greetFromRust = () => {
+    invoke<string>("greet", { name: "Rust" })
+      .then((result) => {
+        console.log("rust called!");
+        setGreet(result);
+      })
+      .catch(console.error);
+  };
+
   return (
     <div className="min-h-screen">
-      <p>hello, router</p>
+      <p>{`hello, ${greet}`}</p>
+      <div className=" flex justify-center h-96">
+        <div className="flex-col flex justify-center">
+          <div>
+            <button
+              type="button"
+              className="bg-slate-600 shadow-md shadow-slate-600 p-2 hover:shadow-slate-700 active:shadow-none m-4 rounded-md w-40 hover:bg-slate-700 active:bg-slate-800 text-lg h-20"
+              onClick={greetFromRust}
+            >
+              Greet from rust
+            </button>
+            <button
+              type="button"
+              className="bg-slate-600 shadow-md shadow-slate-600 p-2 hover:shadow-slate-700 active:shadow-none m-4 rounded-md w-40 hover:bg-slate-700 active:bg-slate-800 text-lg h-20"
+              onClick={() => setGreet("from TS")}
+            >
+              Greet from TS
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

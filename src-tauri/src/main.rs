ab809bb6e33f198dd894638e608160e6ed5dff10
Lsig <logis21@ru.is>
2024-03-29 13:17:56 +0000

    fix(monitor): Remove uneccessary invoke handler

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,16 +4,17 @@ extern crate pcap;
 extern crate pnet;
 
 mod network_monitor;
+use crate::network_monitor::monitor;
 use tauri::Manager;
-use crate::network_monitor::monitor::init_traffic_listener;
 
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
-            init_traffic_listener(app.get_window("main").expect("Failed to get main window"));
+            monitor::init_traffic_listener(
+                app.get_window("main").expect("Failed to get main window"),
+            );
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![init_traffic_listener])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }

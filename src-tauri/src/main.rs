97e4f66c90d72c79a586d722f51d08bc6f4e9b70
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-03-31 13:02:30 +0000

    greet rust func moved to its own module

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-#[tauri::command]
-fn greet(name: &str) -> String {
-    println!("called in rust");
-    return format!("{}!", name);
-}
+mod router;
+use crate::router::find_ip;
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![find_ip::greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src-tauri/src/router/find_ip.rs
@@ -0,0 +1,10 @@
+// #[tauri::command]
+// pub fn find_ip() -> String {
+//     "10.0.0.1".to_string()
+// }
+
+#[tauri::command]
+pub fn greet(name: &str) -> String {
+    println!("called in rust");
+    return format!("{}!", name);
+}

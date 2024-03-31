0bf9d27e122a0a1aa7e14bb43629551940442efb
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-03-31 12:57:20 +0000

    fix: div can't be direct child of <html> element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full bg-gray-900">
-      <div className="absolute border-DDOrange top-0 bg-slate-500 border-t-2 w-screen" />
-      <body className={`${outfit.className} h-full px-24`}>
+    <html lang="en">
+      <body className={`${outfit.className} px-24`}>
+        <div className="absolute border-DDOrange top-0 z-10 left-0 border-t-2 w-screen" />
         <Navbar />
         {children}
       </body>

3a4c5ad8921fbd63714bfb499d27af235d25f18c
Lsig <logis21@ru.is>
2024-04-02 13:24:04 +0000

    feat(info): Make a infotable component

--- src/app/monitor/page.tsx
+++ src/app/monitor/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { RealTimeChart } from "@/components/Chart";
+import { IpTable } from "@/components/InfoTable";
 import { PacketTable } from "@/components/PacketTable";
 
 export default function Monitor() {
   return (
     <div className="px-8">
       <RealTimeChart />
+      <IpTable />
       <PacketTable />
     </div>
   );
---
+++ src/components/InfoTable.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { listen } from "@tauri-apps/api/event";
+
+interface IPCache {
+  [ipAddress: string]: IpInfo;
+}
+
+interface IpInfo {
+  flag: string;
+  country: string;
+  hostname: string;
+  ip: string;
+}
+
+export function IpTable() {
+  const [ips, setIps] = useState<IPCache>({});
+
+  useEffect(() => {
+    const unlisten = listen("info", (e) => {
+      const payload = e.payload as string;
+      const newIps: IPCache = JSON.parse(payload);
+      setIps(() => newIps);
+    });
+
+    return () => {
+      unlisten.then((f) => f());
+    };
+  }, []);
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-4">
+      <div className="overflow-x-auto">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Domain</th>
+              <th>Country</th>
+              <th>Flag</th>
+              <th>Ip</th>
+            </tr>
+          </thead>
+        </table>
+        <div
+          className="overflow-y-auto no-scrollbar"
+          style={{ maxHeight: "200px" }}
+        >
+          <table className="table">
+            <tbody>
+              {Object.values(ips).map((ip) => (
+                <tr className="animate-fadeIn">
+                  <td>{ip.hostname}</td>
+                  <td>{ip.country}</td>
+                  <td>{ip.flag}</td>
+                  <td>{ip.ip}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}

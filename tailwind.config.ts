eebc2259d0ec2fc3dbaea0237182ad48f96dfaa8
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-04-05 00:51:00 +0000

    extend, do not replace color config!

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,12 +19,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        DDOrange: "#F27F15",
+      },
     },
     animation: {
       fadeIn: "fadeIn 1s forwards",
-    },
-    colors: {
-      DDOrange: "#F27F15",
     },
   },
   plugins: [require("daisyui")],

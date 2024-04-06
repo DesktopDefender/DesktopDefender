5f255e5a77cf20985864ec5a816a6f3a39d4deaa
Kristófer Fannar Björnsson <68929760+kristoferfannar@users.noreply.github.com>
2024-04-06 21:55:17 +0000

    style: global component fixes (#13)
    * Page Container type + styling fixes
    
    min height is screen height - children can be a list of JSX.Elements
    
    * navbar & orange top border positions made fixed

--- src/components/DDPageContainer.tsx
+++ src/components/DDPageContainer.tsx
@@ -1,10 +1,10 @@
 interface Props {
-  children: JSX.Element;
+  children: JSX.Element | JSX.Element[];
 }
 
 export default function DDPageContainer({ children }: Props) {
   return (
-    <main className="border-x-2 border-dashed flex flex-col justify-center py-4">
+    <main className="border-x-2 border-dashed flex flex-col justify-center min-h-screen py-4">
       {children}
     </main>
   );

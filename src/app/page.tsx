090490cbe79d5d05979912faf43e6a35bee8afae
halli21 <haraldurf21@ru.is>
2024-04-07 14:41:02 +0000

    created connection component for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,12 @@
 "use client";
 
-import DDText from "@/components/core/DDText";
-import { listen } from "@tauri-apps/api/event";
-import { useEffect, useState } from "react";
+import { ConnectionStatus } from "@/components/ConnectionStatus";
 
 export default function Home() {
-  const [connected, setConnected] = useState<boolean>(false);
-
-  useEffect(() => {
-    listen("connection_status", (e) => {
-      const payload = e.payload as boolean;
-      //const status: boolean = JSON.parse(payload);
-      setConnected(payload);
-    });
-  }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <DDText className="text-3xl">Want me to protect your network?</DDText>
-        {connected ? (
-          <DDText className="text-xl text-center">Call me the defender</DDText>
-        ) : (
-          <DDText className="text-xl text-center">
-            What network little bro? 😂
-          </DDText>
-        )}
-      </div>
+      <ConnectionStatus />
     </main>
   );
 }

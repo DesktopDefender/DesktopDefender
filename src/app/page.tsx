3fab09c36d6f512a9f96081d010b35f57f12d478
halli21 <haraldurf21@ru.is>
2024-04-07 14:56:38 +0000

    format with biome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,13 @@ import { ConnectionStatus } from "@/components/ConnectionStatus";
 import { Card } from "@/components/Card";
 import DDPageContainer from "@/components/DDPageContainer";
 
-
 export default function Home() {
-
   return (
     <DDPageContainer>
       <ConnectionStatus />
       <h1 className="self-center text-3xl font-bold mb-4">Desktop defender</h1>
       <div className="grid grid-cols-2 gap-4">
-        <Card
-          title="Router"
-          body="My router"
-          link="/router"
-        />
+        <Card title="Router" body="My router" link="/router" />
         <Card
           title="Devices"
           body="See which devices are on you network"

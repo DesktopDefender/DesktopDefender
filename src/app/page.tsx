eac79acb0ec26d3f72abd79a1983dbf17873b94c
Logi Sigurðarson <89947902+lsig@users.noreply.github.com>
2024-04-07 14:40:51 +0000

    Feat/home (#16)
    * feat(card): Add a card component for home page
    
    * feat(home): Restructure home page with cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Card } from "@/components/Card";
+import DDPageContainer from "@/components/DDPageContainer";
 import DDText from "@/components/core/DDText";
 import { listen } from "@tauri-apps/api/event";
 import { useEffect, useState } from "react";
@@ -16,17 +18,34 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <DDText className="text-3xl">Want me to protect your network?</DDText>
-        {connected ? (
-          <DDText className="text-xl text-center">Call me the defender</DDText>
-        ) : (
-          <DDText className="text-xl text-center">
-            What network little bro? 😂
-          </DDText>
-        )}
+    <DDPageContainer>
+      <h1 className="self-center text-3xl font-bold mb-4">Desktop defender</h1>
+      <div className="grid grid-cols-2 gap-4">
+        <Card
+          title="Router"
+          body="My router"
+          link="/router"
+          buttonText="Go to Router"
+        />
+        <Card
+          title="Devices"
+          body="See which devices are on you network"
+          link="/devices"
+          buttonText="See Devices"
+        />
+        <Card
+          title="Traffic"
+          body="See statistics about your network traffic and usage"
+          link="/monitor"
+          buttonText="See Traffic"
+        />
+        <Card
+          title="Settings"
+          body="Configure Desktop Defender to your liking"
+          link="/"
+          buttonText="Go to Settings"
+        />
       </div>
-    </main>
+    </DDPageContainer>
   );
 }
